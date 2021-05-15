e77534a00f626fe7d4af4b8925738058babb3749
Maksym Lemich <lemichello.m@gmail.com>
2021-05-15 17:42:25 +0300

    moved user schema to the db folder

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { usersServiceFactoryProvider } from './providers/users-service.provider';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
+import { User, UserSchema } from './db/schemas/user.schema';
 
 @Module({
   imports: [
