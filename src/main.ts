ac6b9bfd6feb094803f9b9b37d2911dc7f05a1a6
Maksym Lemich <lemichello.m@gmail.com>
2021-05-15 16:42:32 +0300

    added basic configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupBaseConfiguration(app);
+
   await app.listen(3000);
 }
+
+function setupBaseConfiguration(app: INestApplication) {
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.use(helmet());
+}
+
 bootstrap();
