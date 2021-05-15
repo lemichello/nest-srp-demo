6501e0f7846617f8aea951485fa2da791eef82d6
Maksym Lemich <lemichello.m@gmail.com>
2021-05-15 16:46:45 +0300

    added mongoose integration

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_MONGO_CONNECTION_STRING as string),
+  ],
   controllers: [],
   providers: [],
 })
