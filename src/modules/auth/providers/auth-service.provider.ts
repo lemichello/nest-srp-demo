6378de64bbacd25044fed0abdda748275c90555f
Maksym Lemich <lemichello.m@gmail.com>
2021-05-16 11:52:39 +0300

    fixed instantiating auth service

--- src/modules/auth/providers/auth-service.provider.ts
+++ src/modules/auth/providers/auth-service.provider.ts
@@ -1,13 +1,14 @@
 import { FactoryProvider } from '@nestjs/common';
 import { AuthService } from '../services/auth/auth.service';
-import { AUTH_SERVICE } from '../auth.constants';
+import { AUTH_SERVICE, SRP_SERVICE } from '../auth.constants';
 import { USERS_SERVICE } from '../../users/users.constants';
 import { IUsersService } from '../../users/interfaces/users-service.interface';
+import { ISrpService } from '../interfaces/srp-service.interface';
 
 export const authServiceFactoryProvider: FactoryProvider<AuthService> = {
   provide: AUTH_SERVICE,
-  useFactory: (usersService: IUsersService) => {
-    return new AuthService(usersService);
+  useFactory: (usersService: IUsersService, srpService: ISrpService) => {
+    return new AuthService(usersService, srpService);
   },
-  inject: [USERS_SERVICE],
+  inject: [USERS_SERVICE, SRP_SERVICE],
 };
